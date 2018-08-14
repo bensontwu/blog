6a9fd7200a12e9371a10ffde46ecde9a9181580c
bensontwu <benson.wu@handlefin.com>
2018-08-14 12:01:57 -0700

    Added nav links and title to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-	get 'static_pages/home'
-	get 'static_pages/about'
-	get 'posts', to: 'posts#index'
+	root 'static_pages#home'
+	get '/home', to: 'static_pages#home'
+	get '/about', to: 'static_pages#about'
+	get '/posts', to: 'posts#index'
 	resources :news
   	devise_for :users
-	root 'static_pages#home'
 	resources :posts
 end
