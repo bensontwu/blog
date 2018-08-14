92e4fb2382aa0844449222081a2e0c51f5773cb6
bensontwu <benson.wu@handlefin.com>
2018-08-14 14:57:59 -0700

    Fix navbar link again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 	root 'static_pages#home'
 	get '/home', to: 'static_pages#home'
 	get '/about', to: 'static_pages#about'
-	get '/posts', to: 'posts#index'
 	resources :news
   	devise_for :users
 	resources :posts
